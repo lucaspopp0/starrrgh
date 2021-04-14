672202a28e6d6ca07119d85dd6077e3e301076ec
Lucas Popp <lmp122@case.edu>
2021-04-13 21:29:29 -0400

    Add buttons

---
+++ Assets/Scripts/UI/MyButton.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(Button))]
+public class MyButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler {
+
+    [SerializeField] private RectTransform textTransform;
+    private Button _button;
+    private float _height;
+    private float _offset;
+
+    private void Awake() {
+        _button = GetComponent<Button>();
+        var rt = (RectTransform) transform;
+        _offset = rt.rect.height * transform.localScale.y / 12f;
+    }
+
+    public void OnPointerDown(PointerEventData eventData) {
+        textTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, _offset, textTransform.rect.height);
+    }
+
+    public void OnPointerUp(PointerEventData eventData) {
+        textTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, 0f, textTransform.rect.height);
+    }
+
+}
