96da7a1a700de1a3e12d96b3dee8080bf2151c13
prao3 <44528013+prao3@users.noreply.github.com>
2021-04-15 14:35:45 -0400

    Player movement (#1)
    * Adding a script to keep track of planet mass
    
    Signed-off-by: prao3 <44528013+prao3@users.noreply.github.com>
    
    * Adding player movement script
    
    Signed-off-by: prao3 <44528013+prao3@users.noreply.github.com>
    
    * Adding a test scene
    
    Signed-off-by: prao3 <44528013+prao3@users.noreply.github.com>
    
    * Setting up the movement scene
    
    Signed-off-by: prao3 <44528013+prao3@users.noreply.github.com>
    
    * Adding some instructions in the movement scene
    
    Signed-off-by: prao3 <44528013+prao3@users.noreply.github.com>
    
    * Getting the camera to follow the player
    
    Signed-off-by: prao3 <44528013+prao3@users.noreply.github.com>
    
    * Capping the velocity of the ship
    
    Signed-off-by: prao3 <44528013+prao3@users.noreply.github.com>
    
    * Actually merging in master, whoops
    
    Signed-off-by: prao3 <44528013+prao3@users.noreply.github.com>
    
    * Add player ship
    
    Co-authored-by: Lucas Popp <lmp122@case.edu>

---
+++ Assets/Scripts/Planet.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Planet : MonoBehaviour
+{
+    [SerializeField] private float mass;
+
+    public float getMass()
+    {
+        return mass;
+    }
+}
