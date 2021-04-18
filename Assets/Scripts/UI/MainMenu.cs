2fa5eca31a72781a88cd637aa14d95078bc4be1b
Lucas Popp <lmp122@case.edu>
2021-04-18 15:06:40 -0400

    Create instructions and leaderboard menus

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -1,16 +1,33 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
+    [SerializeField] private MainMenuPanel instructionsMenu;
+    [SerializeField] private MainMenuPanel leaderboardMenu;
+
+    private void Awake() {
+        instructionsMenu.Close();
+        leaderboardMenu.Close();
+    }
+
     public void StartGame() {
         SceneManager.LoadScene(SceneId.MainGame);
+    }
+
+    public void OpenLeaderboard() {
+        leaderboardMenu.Open();
+    }
+
+    public void OpenInstructions() {
+        instructionsMenu.Open();
     }
 
     public void Quit() {
         Application.Quit();
     }
-    
+
 }
