8ffebb1ecc97cca7476a7bc00f04720a67a8360a
Adam Stewart <adamjoesph11@gmail.com>
2021-04-19 17:27:18 -0400

    Adding scripts for aiming and shooting with the ship

---
+++ Assets/Scripts/Player Scripts/PlayerAim.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAim : MonoBehaviour
+{
+    private Animator shootAnimator;
+    public Transform AimGunEndPointTransform;
+ 
+    public event EventHandler<OnShootEventArgs> OnShoot;
+    public class OnShootEventArgs : EventArgs
+    {
+        public Vector3 gunEndPointPosition;//What angle the bullet will be firing from
+        public Vector3 shootPosition;
+    }
+    //Find the mouse position in the world with Z = 0
+    public static Vector3 GetMouseWorldPosition()
+    {
+        Vector3 vec = GetMouseWorldPositionWithZ(Input.mousePosition, Camera.main);
+        vec.z = 0f;
+        return vec;
+    }//If we want to have some sort of weapon  rotation it will need to be in respect to Z only 
+
+    public static Vector3 GetMouseWorldPositionWithZ(Vector3 mousePosition, Camera worldCamera)
+    {
+        Vector3 worldPosition = worldCamera.ScreenToWorldPoint(mousePosition);
+        return worldPosition;
+    }
+
+    private void HandleShooting()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = GetMouseWorldPosition();
+           // shootAnimator.SetBool("Shoot",true);
+            OnShoot?.Invoke(this,new OnShootEventArgs
+            {
+                gunEndPointPosition = AimGunEndPointTransform.position,
+                shootPosition = mousePosition,
+     
+            });
+   
+        }
+
+    }
+
+    private void Update()
+    {
+        HandleShooting();
+    }
+}
---
+++ Assets/Scripts/Player Scripts/PlayerShoot.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    [SerializeField] private PlayerAim playerAim;
+    [SerializeField] private Material projectile;
+
+    private void Start()
+    {
+        playerAim.OnShoot += PlayerShoot_OnShoot;
+    }
+    
+
+    private void PlayerShoot_OnShoot(object sender, PlayerAim.OnShootEventArgs e)
+    {
+        Debug.Log("Shoot");
+        Debug.DrawLine(e.gunEndPointPosition,e.shootPosition,Color.white,5,true);
+        CreateWeaponTracer(e.gunEndPointPosition, e.shootPosition);
+    }
+
+    public static float GetAngleFromVectorFloat(Vector3 dir) {
+        dir = dir.normalized;
+        float n = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        if (n < 0) n += 360;
+
+        return n;
+    }
+
+    
+    private void CreateWeaponTracer(Vector3 fromPosition, Vector3 targetPosition)
+    {//If we want to restrict the angle of fire we can add a check for it here
+        float distance = Vector3.Distance(fromPosition, targetPosition);
+        Vector3 dir = (targetPosition - fromPosition).normalized;
+        Vector3 tracerSpawnPosition = fromPosition + dir * distance * .5f;
+        float zRot = GetAngleFromVectorFloat(dir) - 90;
+        Material tmp = new Material(projectile);
+        tmp.SetTextureScale("_MainTex",new Vector2(1f,distance/256f));
+        World_Mesh worldMesh = World_Mesh.Create(tracerSpawnPosition,zRot,.5f,distance,projectile,null,10000);
+         float timer = .1f;
+         
+         FunctionUpdater.Create(() =>
+         {
+             timer -= Time.deltaTime;
+             if (timer <= 0)
+             {
+                 worldMesh.DestroySelf();
+                 return true;
+             }
+
+             return false;
+         });
+    }
+
+}
