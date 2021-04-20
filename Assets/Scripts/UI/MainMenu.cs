cc4e54fb76761431ea3a7fa3eeb981a573136246
Lucas Popp <lmp122@case.edu>
2021-04-20 11:35:33 -0400

    Add stars to main menu

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -6,12 +6,31 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour {
 
+    private const float StarsMovementSpeed = 0.3f;
+
+    [SerializeField] private RectTransform stars;
     [SerializeField] private MainMenuPanel instructionsMenu;
     [SerializeField] private MainMenuPanel leaderboardMenu;
+
+    private float _starsLoopProgress = 0f;
 
     private void Awake() {
         instructionsMenu.Close();
         leaderboardMenu.Close();
+        Debug.Log(stars.rect.height);
+        Debug.Log(stars.transform.position.y);
+    }
+
+    private void Update() {
+        // Loop stars background
+        var inset = _starsLoopProgress * stars.rect.height;
+        stars.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Top, inset, stars.rect.height);
+        
+        _starsLoopProgress += Time.deltaTime * StarsMovementSpeed;
+        
+        if (_starsLoopProgress >= 0.999f) {
+            _starsLoopProgress = 0f;
+        }
     }
 
     public void StartGame() {
