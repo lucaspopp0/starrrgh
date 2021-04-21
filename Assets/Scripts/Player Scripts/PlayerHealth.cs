42fd9532d1008c8a7054049be178ad3a2a517156
Lucas Popp <lmp122@case.edu>
2021-04-21 15:42:54 -0400

    Update player color with damage

--- Assets/Scripts/Player Scripts/PlayerHealth.cs
+++ Assets/Scripts/Player Scripts/PlayerHealth.cs
@@ -1,16 +1,25 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHealth : MonoBehaviour
-{
-	[SerializeField] private Hud _hud;
+public class PlayerHealth : MonoBehaviour {
+
+	[SerializeField] private Gradient damageColorGradient;
+	[SerializeField] private SpriteRenderer shipSpriteRenderer;
+		
+	private Hud _hud;
     private int _health;
     private static int MAX_HEALTH = 1000;
 
-	void Start() {
+    private void Awake() {
+	    _hud = GameObject.FindWithTag("HUD").GetComponent<Hud>();
+    }
+
+    void Start() {
 		_health = MAX_HEALTH;
-	}
+		shipSpriteRenderer.color = damageColorGradient.Evaluate(0);
+    }
 
 	void Update(){
 		if(_health == 0){
@@ -21,11 +30,13 @@ public class PlayerHealth : MonoBehaviour
 	public void Hurt(int damage) {
 		_health -= damage;
 		_hud.healthBar.SetNormalizedValue(_health / (float)MAX_HEALTH);
+		shipSpriteRenderer.color = damageColorGradient.Evaluate(1f - _health / (float) MAX_HEALTH);
 	}
 
 	public void Die() {
 		_health = 0;
 		_hud.healthBar.SetNormalizedValue(0);
+		shipSpriteRenderer.color = damageColorGradient.Evaluate(1);
 		_hud.PlayerDied();
 		GetComponent<PlayerMovement>().kill();
 	}
