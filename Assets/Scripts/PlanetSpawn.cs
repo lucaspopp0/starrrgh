f9867de26a881509cc7235c711e0870454efe971
prao3 <44528013+prao3@users.noreply.github.com>
2021-04-22 15:46:18 -0400

    Adding stuff to spawn features in random locations upon startup
    Signed-off-by: prao3 <44528013+prao3@users.noreply.github.com>

--- Assets/Scripts/PlanetSpawn.cs
+++ Assets/Scripts/PlanetSpawn.cs
@@ -5,19 +5,35 @@ using UnityEngine;
 public class PlanetSpawn : MonoBehaviour
 {
     [SerializeField] private float spawnRadius = 100.0f;
+    [SerializeField] private float minSpawnRadius = 10.0f;
     [SerializeField] private int numObjects = 10;
-    [SerializeField] private GameObject[] objects;
+    [SerializeField] private GameObject[] prefabs;
     [SerializeField] private GameObject player;
+
+    private GameObject[] spawnedObjects;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        spawnedObjects = new GameObject[numObjects];
+
+        for (int i = 0; i < numObjects; i++)
+        {
+            float distance = Random.Range(minSpawnRadius, spawnRadius);
+            Vector3 v = Random.insideUnitCircle.normalized * distance;
+            GameObject o = Instantiate(randomPrefab(), v, Quaternion.identity);
+            spawnedObjects[i] = o;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private GameObject randomPrefab()
+    {
+        return prefabs[Random.Range(0, prefabs.Length - 1)];
     }
 }
