1b4e3b6f0203fce04c1c0fdae194a23e00803687
prao3 <44528013+prao3@users.noreply.github.com>
2021-04-22 17:18:55 -0400

    Adding some comments to PlanetSpawn
    Signed-off-by: prao3 <44528013+prao3@users.noreply.github.com>

--- Assets/Scripts/PlanetSpawn.cs
+++ Assets/Scripts/PlanetSpawn.cs
@@ -19,15 +19,20 @@ public class PlanetSpawn : MonoBehaviour
         spawnedObjects = new GameObject[numObjects];
         movement = player.GetComponent<PlayerMovement>();
 
+        //Spawn a feature for the number specified, and then add any planets to the player movement script
         for (int i = 0; i < numObjects; i++)
         {
+            //Spawning at some random vector
             float distance = Random.Range(minSpawnRadius, spawnRadius);
             Vector3 v = Random.insideUnitCircle.normalized * distance;
+            //Maybe rotate prefabs to some random direction
             GameObject o = Instantiate(randomPrefab(), v, Quaternion.identity);
             spawnedObjects[i] = o;
 
+            //Adding all planets in the feature to the player movement script
             foreach(Transform child in o.transform)
             {
+                //Probably don't have to do this check since we do it in addPlanet
                 if(child.gameObject.GetComponent<Planet>() != null)
                 {
                     movement.addPlanet(child.gameObject);
@@ -39,9 +44,15 @@ public class PlanetSpawn : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //Check if planets are still within spawn distance of the ship
+
+        //If a planet is outside that range, despawn it
+        //Then spawn a new planet in the direction the ship is moving
     }
 
+    /*
+     * Returns a random object from our prefab list
+     */
     private GameObject randomPrefab()
     {
         return prefabs[Random.Range(0, prefabs.Length)];
