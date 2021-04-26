84777ce41c931f0a5c0f6fa1000a38541288da91
Alex Petruso <asp125@case.edu>
2021-04-26 14:14:52 -0400

    AI testing scene

--- Assets/Scripts/EnemyLazer.cs
+++ Assets/Scripts/EnemyLazer.cs
@@ -21,7 +21,7 @@ public class EnemyLazer : MonoBehaviour
         Debug.Log("hit somthing");
 		PlayerHealth player = other.GetComponent<PlayerHealth>();
 		if (player != null) {
-			player.Hurt(5);
+			player.Hurt(10);
 		}
         if(other.GetComponent<ReactiveTarget>()){
             other.GetComponent<ReactiveTarget>().ReactToHit();
