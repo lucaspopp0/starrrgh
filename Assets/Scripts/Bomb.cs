102a4b9babb99de2dd9551a7e24db3ae47b43b0e
Adam Stewart <adamjoesph11@gmail.com>
2021-04-26 15:03:03 -0400

    Adding basic bomb scripting and gameObject

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    [SerializeField] private float fuseTimer = 3f;
+    private bool fuseEnabled = false;
+
+    private void Update()
+    {
+        if (fuseEnabled)
+        {
+            fuseTimer -= Time.deltaTime;
+        }
+
+
+        if (fuseTimer <= 0)
+        {
+            Detonate();
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.gameObject.GetComponent<PlayerHealth>().Hurt(500);
+        }
+        if (other.gameObject.tag == "Police")
+        {
+            //TODO: Add police tag
+            other.gameObject.GetComponent<WanderingAI>().SetAlive(false);
+        }
+    }
+
+    public void EnableFuse()
+    {
+        fuseEnabled = true;
+    }
+
+    private void Detonate()
+    {
+        Debug.Log("boom");
+        //TODO: Check for collision on children here after enabling them
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Player Scripts/PlayerBomb.cs
+++ Assets/Scripts/Player Scripts/PlayerBomb.cs
@@ -1,14 +1,28 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerBomb : MonoBehaviour
 {
     private int bombAmount = 0;
+    [SerializeField] private GameObject _bombPrefab;
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && bombAmount > 0)
+        {
+            GameObject bomb = Instantiate(_bombPrefab);
+            bomb.GetComponent<Bomb>().EnableFuse();
+            bombAmount--;
+        }
+    }
 
     public void AddBombs(int amount)
     {
         bombAmount += amount;
+        Debug.Log(bombAmount);
     }
 
     public void UseBomb()
