9e828814963940322c47e12f97378e3ba5e21584
Lucas Popp <lucaspopp0@gmail.com>
2021-04-26 14:21:24 -0400

    Add damage indicators for sprites and stuff (#4)
    * Update ship sprites
    
    * Update player color with damage
    
    * Undo changes to main scene
    
    * Update sprite names

--- Assets/Scripts/Player Scripts/PlayerHealth.cs
+++ Assets/Scripts/Player Scripts/PlayerHealth.cs
@@ -1,23 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHealth : MonoBehaviour
-{
-	[SerializeField] private Hud _hud;
+public class PlayerHealth : MonoBehaviour {
+
+	[SerializeField] private Gradient damageColorGradient;
+	[SerializeField] private SpriteRenderer shipSpriteRenderer;
+		
+	private Hud _hud;
     private int _health;
     private static int MAX_HEALTH = 1000;
-
     private bool _shielded = false;
     private float _shieldTimer = 0f;
     private static float MAX_SHIELD_TIME = 3f;
     
     [SerializeField] private GameObject explosionEffect;
 
+    private void Awake() {
+	    _hud = GameObject.FindWithTag("HUD").GetComponent<Hud>();
+    }
 
     void Start() {
 		_health = MAX_HEALTH;
-	}
+		shipSpriteRenderer.color = damageColorGradient.Evaluate(0);
+    }
 
 	void Update(){
 		if(_health == 0){
@@ -33,6 +40,7 @@ public class PlayerHealth : MonoBehaviour
 	public void Hurt(int damage) {
 		_health -= damage;
 		_hud.healthBar.SetNormalizedValue(_health / (float)MAX_HEALTH);
+		shipSpriteRenderer.color = damageColorGradient.Evaluate(1f - _health / (float) MAX_HEALTH);
 	}
 
 	public void Heal(int amount)
@@ -46,9 +54,10 @@ public class PlayerHealth : MonoBehaviour
 		{
 			_health = 0;
 			_hud.healthBar.SetNormalizedValue(0);
+			shipSpriteRenderer.color = damageColorGradient.Evaluate(1);
 			_hud.PlayerDied();
 			GetComponent<PlayerMovement>().kill();
-      explosionEffect.SetActive(true);
+			explosionEffect.SetActive(true);
 		}
 		else
 		{
