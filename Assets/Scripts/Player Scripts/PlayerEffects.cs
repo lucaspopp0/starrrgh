cc5f028d099a5a3abc028c8f7261f56530520462
prao3 <44528013+prao3@users.noreply.github.com>
2021-05-01 13:33:52 -0400

    Decreasing aberration with time after the player dashes
    Signed-off-by: prao3 <44528013+prao3@users.noreply.github.com>

--- Assets/Scripts/Player Scripts/PlayerEffects.cs
+++ Assets/Scripts/Player Scripts/PlayerEffects.cs
@@ -6,33 +6,40 @@ using UnityEngine.Rendering.PostProcessing;
 public class PlayerEffects : MonoBehaviour
 {
     [SerializeField] private Camera mainCamera;
+    [SerializeField] private float aberrationTime = 1.0f;
     private PostProcessVolume volume;
+    private ChromaticAberration aberration;
+
+    private float intensity = 1.0f;
+    private bool chromaticEffect = false;
     // Start is called before the first frame update
     void Start()
     {
         volume = mainCamera.GetComponent<PostProcessVolume>();
+        volume.profile.TryGetSettings<ChromaticAberration>(out aberration);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (chromaticEffect && aberration != null)
+        {
+            intensity -= Time.deltaTime / aberrationTime;
+            aberration.intensity.value = intensity;
+            if(intensity <= 0)
+            {
+                chromaticEffect = false;
+            }
+        }
+        else if (!chromaticEffect)
+        {
+            intensity = 1.0f;
+        }
     }
 
     public void boostEffect()
     {
-        StartCoroutine(chromaticAberration());
-    }
-
-    IEnumerator chromaticAberration()
-    {
-        ChromaticAberration aberration;
-        volume.profile.TryGetSettings<ChromaticAberration>(out aberration);
-        float intensity = 1;
-        aberration.intensity.Override(intensity);
-
-        yield return new WaitForSeconds(1);
-
-        aberration.intensity.Override(0);
+        chromaticEffect = true;
+        intensity = 1.0f;
     }
 }
