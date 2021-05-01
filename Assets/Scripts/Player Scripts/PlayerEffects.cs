ae5f6f1fe34e08f3dea1f3ae5f4939b57a8553b2
prao3 <44528013+prao3@users.noreply.github.com>
2021-05-01 16:15:06 -0400

    Adding some camera zoom when the player charges boost
    Signed-off-by: prao3 <44528013+prao3@users.noreply.github.com>

--- Assets/Scripts/Player Scripts/PlayerEffects.cs
+++ Assets/Scripts/Player Scripts/PlayerEffects.cs
@@ -14,11 +14,17 @@ public class PlayerEffects : MonoBehaviour
     private float intensity = 0.0f;
     private bool releaseChromatic = false;
     private bool chargeChromatic = false;
+
+    private float cameraSize;
+    private float targetZoom = 4.0f;
+    private float zoomSpeed = 1.0f;
+    private float time = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
         volume = mainCamera.GetComponent<PostProcessVolume>();
         volume.profile.TryGetSettings<ChromaticAberration>(out aberration);
+        cameraSize = mainCamera.orthographicSize;
     }
 
     // Update is called once per frame
@@ -27,6 +33,8 @@ public class PlayerEffects : MonoBehaviour
         intensity = Mathf.Clamp(intensity, 0, 1);
         if (Input.GetKey(KeyCode.Space) && intensity != 1)
         {
+            mainCamera.orthographicSize = cameraSize + 0.5f - sigmoid(time, (cameraSize - targetZoom) * 2, zoomSpeed);
+            time += Time.deltaTime;
             intensity += Time.deltaTime * aberrationChargeSpeed;
             aberration.intensity.value = intensity;
         }
@@ -35,5 +43,16 @@ public class PlayerEffects : MonoBehaviour
             intensity -= Time.deltaTime / aberrationReleaseTime;
             aberration.intensity.value = intensity;
         }
+        
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            time = 0.0f;
+            mainCamera.orthographicSize = cameraSize;
+        }
+    }
+
+    private float sigmoid(float t, float a, float b)
+    {
+        return b / (1 + Mathf.Exp(-a * t));
     }
 }
