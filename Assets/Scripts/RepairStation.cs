40b14c8987eb8beed88b4bcd02235a7d1e229e13
Lucas Popp <lmp122@case.edu>
2021-05-06 22:22:07 -0400

    Repair station UI:

--- Assets/Scripts/RepairStation.cs
+++ Assets/Scripts/RepairStation.cs
@@ -4,20 +4,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class RepairStation : MonoBehaviour
-{
+public class RepairStation : MonoBehaviour {
+
+    private const int HEALTH_COST = 25;
     [SerializeField] private float baseWaitTime = 2f;
     private float timer;
     private float waitBeforeRepairing = 0f;
     private bool isWaiting = false;
     private bool isRepairing = false;
     [SerializeField] private int repairRate = 50;//How much HP is restored per second
+    [SerializeField] private StationUI stationUI;
 
     private ScoreController _scoreController;
     private void Start()
     {
         _scoreController = GameObject.Find("Score Controller").GetComponent<ScoreController>();
         waitBeforeRepairing = baseWaitTime;
+        UpdateUI();
     }
 
     private void Update()
@@ -25,6 +28,7 @@ public class RepairStation : MonoBehaviour
         if (isWaiting)
         {
             waitBeforeRepairing -= Time.deltaTime;
+            stationUI.slider.normalizedValue = (baseWaitTime - waitBeforeRepairing + timer) / (baseWaitTime + 1f);
         }
 
         if (waitBeforeRepairing <= 0)
@@ -33,28 +37,51 @@ public class RepairStation : MonoBehaviour
         }
     }
 
+    private void UpdateUI(PlayerHealth playerHealth = null) {
+        if (playerHealth == null) {
+            stationUI.gameObject.SetActive(false);
+        } else {
+            stationUI.gameObject.SetActive(true);
+
+            if (playerHealth.GetHealth() < PlayerHealth.MAX_HEALTH && _scoreController.GetScore() >= HEALTH_COST) {
+                stationUI.slider.gameObject.SetActive(true);
+                stationUI.slider.normalizedValue = (baseWaitTime - waitBeforeRepairing + timer) / (baseWaitTime + 1f);
+                stationUI.priceText.text = $"{HEALTH_COST} loot = {repairRate * 100 / PlayerHealth.MAX_HEALTH}% health";
+            } else {
+                stationUI.slider.gameObject.SetActive(false);
+                stationUI.priceText.text = _scoreController.GetScore() < HEALTH_COST ? "Need more loot" : "All healed!";
+            }
+        }
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Player")
-        {
+        if (other.gameObject.tag == "Player") {
+            timer = 0f;
             BeginWait();
+            waitBeforeRepairing = baseWaitTime;
+            UpdateUI(other.GetComponent<PlayerHealth>());
         }
     }
 
     private void OnTriggerStay2D(Collider2D other)
     {
-        timer += Time.deltaTime;
+        if (waitBeforeRepairing <= 0) {
+            timer += Time.deltaTime;
+        }
 
-        if (other.gameObject.tag == "Player" && isRepairing)
+        if (other.gameObject.tag == "Player")
         {
             PlayerHealth playerHealth = other.gameObject.GetComponent<PlayerHealth>();
-            if (playerHealth.GetHealth() < PlayerHealth.MAX_HEALTH && timer >= 1 && _scoreController.GetScore() > 0)
-            {
+            if (playerHealth.GetHealth() < PlayerHealth.MAX_HEALTH && timer >= 1 && _scoreController.GetScore() >= HEALTH_COST) {
                 timer = 0;
-                _scoreController.SubScore(25);
                 playerHealth.Heal(repairRate);
-
+                _scoreController.SubScore(HEALTH_COST);
+                waitBeforeRepairing = baseWaitTime;
+                isRepairing = false;
+                isWaiting = true;
             }
+            UpdateUI(playerHealth);
         }    
     }
 
@@ -66,6 +93,8 @@ public class RepairStation : MonoBehaviour
             isRepairing = false;
             isWaiting = false;
             waitBeforeRepairing = baseWaitTime;
+            stationUI.gameObject.SetActive(false);
+            UpdateUI();
         }
         
     }
---
+++ Assets/Scripts/UI/StationUI.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class StationUI: MonoBehaviour {
+
+        public TMP_Text priceText;
+        public Slider slider;
+
+}
